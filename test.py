9778cdd36bfed0e8c13dcb781073143beccba83c
sheikhmzaman <52789651+sheikhmzaman@users.noreply.github.com>
2020-03-31 13:44:35 -0500

    test.py added

---
+++ test.py
@@ -0,0 +1,28 @@
+def computepay(hour,rate):
+    h = hour
+    r = rate
+    pay = h*r
+    if h> 40.0:
+        reghr=40.0
+        overtmhr=h-40.0
+        regrt=rate
+        overtmrate=regrt*1.5
+        return (reghr*regrt)+(overtmhr*overtmrate)
+    return (pay)
+    
+hrs = input("Enter Hours:")
+rt = input("Hourly rate:")
+try:
+    hour = float(hrs)
+except:
+    print("Not a number, Please try again")
+    quit()
+try:
+    rate = float(rt)
+except:
+    print("Not a number, Please try again")
+    quit()
+
+p = computepay(hour,rate)
+print("Pay",p)
+
